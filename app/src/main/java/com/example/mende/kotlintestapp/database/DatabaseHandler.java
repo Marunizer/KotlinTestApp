c5252b3768256db17be1841ab9ad5a8ddfd4dae7
marunizer <mendez1226@gmail.com>
2018-10-13 21:21:20 -0400

    Added Realistic Models to use replace current models later on Started setting up project for MongoDB

---
+++ app/src/main/java/com/example/mende/kotlintestapp/database/DatabaseHandler.java
@@ -0,0 +1,20 @@
+package com.example.mende.kotlintestapp.database;
+
+public class DatabaseHandler {
+
+//    final StitchAppClient client = Stitch.getDefaultAppClient();
+//client.getAuth().loginWithCredential(new AnonymousCredential()).addOnCompleteListener(
+//    new OnCompleteListener<StitchUser>() {
+//        @Override
+//        public void onComplete(@NonNull final Task<StitchUser> task) {
+//            if (task.isSuccessful()) {
+//                Log.d("myApp", String.format(
+//                        "logged in as user %s with provider %s",
+//                        task.getResult().getId(),
+//                        task.getResult().getLoggedInProviderType()));
+//            } else {
+//                Log.e("myApp", "failed to log in", task.getException());
+//            }
+//        }
+//    });
+}
---
+++ app/src/main/java/com/example/mende/kotlintestapp/views/DownloadTestActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mende.kotlintestapp.views
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.mende.kotlintestapp.R
+
+class DownloadTestActivity: AppCompatActivity(){
+
+    private val TAG = HomeActivity::class.java.simpleName
+    private var EMPTY = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        onInit()
+
+    }
+
+    private fun onInit() {
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
